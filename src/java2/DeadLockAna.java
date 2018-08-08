52613222483cc1afca15694e890edf02762a9ca5
Ningyq <1554115736@qq.com>
2018-08-08 21:58:44 +0800

    change2

---
+++ src/java2/DeadLockAna.java
@@ -0,0 +1,47 @@
+package java2;
+
+/**
+ * Created by Coldwarm on 2018/8/8.
+ */
+public class DeadLockAna {
+    public static void main(String[] args) {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        Object obj3 = new Object();
+
+        Thread t1 = new Thread(new LockThread(obj1,obj2),"t1");
+        Thread t2 = new Thread(new LockThread(obj2,obj3),"t2");
+        Thread t3 = new Thread(new LockThread(obj3,obj1),"t3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class LockThread implements Runnable {
+    private Object obj1;
+    private Object obj2;
+
+    public LockThread(Object obj1, Object obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " requiring lock on  " + obj1);
+        synchronized (obj1) {
+            System.out.println(name + " required lock on  " + obj1);
+
+            System.out.println(name + " requiring lock on  " + obj2);
+            synchronized (obj2) {
+                System.out.println(name + " required lock on  " + obj2);
+            }
+            System.out.println(name + " released lock on  " + obj2);
+        }
+        System.out.println(name + " released lock on  " + obj1);
+        System.out.println(name + " execution finished.");
+    }
+}
---
+++ src/java2/ThreadTimer.java
@@ -0,0 +1,31 @@
+package java2;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Coldwarm on 2018/8/8.
+ */
+public class ThreadTimer extends TimerTask {
+
+    @Override
+    public void run() {
+        System.out.println("Timer task started at " + new Date());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Timer task ended at " + new Date());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TimerTask task = new ThreadTimer();
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(task,0,7*1000);
+        System.out.println("Started");
+        Thread.sleep(50000);
+        timer.cancel();
+    }
+}
