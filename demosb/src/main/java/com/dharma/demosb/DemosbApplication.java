670d4cdc6ac167b7a87ee63392cf94ec77803c45
Ningyq <1554115736@qq.com>
2018-10-01 16:44:34 +0800

    add sb1

--- demosb/src/main/java/com/dharma/demosb/DemosbApplication.java
+++ demosb/src/main/java/com/dharma/demosb/DemosbApplication.java
@@ -1,12 +1,33 @@
 package com.dharma.demosb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@ServletComponentScan
 @SpringBootApplication
 public class DemosbApplication {
 
+    @Value("${dharma.upload.path}")
+    String filePath;
+
     public static void main(String[] args) {
         SpringApplication.run(DemosbApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/upload/**")
+                        .addResourceLocations("file:" + filePath);
+            }
+        };
     }
 }
---
+++ demosb/src/main/java/com/dharma/demosb/controller/WebController.java
@@ -0,0 +1,28 @@
+package com.dharma.demosb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+
+    @GetMapping("")
+    public String index(ModelMap map) {
+        map.put("title", "GUO QING KUAI LE");
+        return "index";
+    }
+
+    @GetMapping("/upload")
+    public String upload(ModelMap map) {
+        return "upload";
+    }
+
+    @RequestMapping("/error")
+    public void error(ModelMap map) {
+        throw new RuntimeException("devision exception");
+    }
+}
